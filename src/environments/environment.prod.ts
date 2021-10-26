5e4fc933da7ea29ea8763e451743a392994c20da
pbl.gllgs <pbl.gllgs@gmail.com>
2021-10-26 12:38:43 -0300

    Initial Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoicGJsZ2xsZ3MiLCJhIjoiY2t2ODdyYmY2OXI4MTJ2bnpwemcyaW9ucCJ9.8gPGDQeu1mpRYeqIFa942w'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoicGJsZ2xsZ3MiLCJhIjoiY2t2ODdyYmY2OXI4MTJ2bnpwemcyaW9ucCJ9.8gPGDQeu1mpRYeqIFa942w'
 };
 
 /*
